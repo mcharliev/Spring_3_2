bb04d260db1a11ac67324bb653a95c14281b5feb
Merdan Charliev <mcharliev@gmail.com>
2022-11-23 16:42:53 +0300

    Homework_3_2!

--- src/main/java/com/skypro/employee/service/EmployeeService.java
+++ src/main/java/com/skypro/employee/service/EmployeeService.java
@@ -9,5 +9,48 @@ import java.util.stream.Collectors;
 
 @Service
 public class EmployeeService {
+    private final Map<Integer, Employee> employees = new HashMap<>();
 
+    public Collection<Employee> getAllEmployees() {
+        return this.employees.values();
+    }
+
+    public Employee addEmployee(EmployeeRequest employeeRequest) {
+        if (employeeRequest.getFirstName() == null || employeeRequest.getLastName() == null) {
+            throw new IllegalArgumentException("Employee name should be set");
+        }
+        Employee employee = new Employee(employeeRequest.getFirstName(),
+                employeeRequest.getLastName(),
+                employeeRequest.getDepartment(),
+                employeeRequest.getSalary());
+        this.employees.put(employee.getId(),employee);
+        return employee;
+    }
+    public int getSalarySum(){
+        return employees.values().stream()
+                .mapToInt(Employee::getSalary)
+                .sum();
+    }
+    public Employee getEmployeeSalaryMin(){
+        return employees.values().stream()
+                .min(Comparator.comparing(Employee::getSalary))
+                .get();
+    }
+    public Employee getEmployeeSalaryMax(){
+        return employees.values().stream()
+                .max(Comparator.comparing(Employee::getSalary))
+                .get();
+    }
+    public List<Employee> getEmployeeHighSalary(){
+        double averageSalary = employees.values().stream()
+                .mapToDouble(Employee::getSalary)
+                .average()
+                .getAsDouble();
+        return employees.values().stream()
+                .filter(e->e.getSalary()>averageSalary)
+                .collect(Collectors.toList());
+
+
+
+    }
 }
